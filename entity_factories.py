b774b04b8f96cbbb984e9635893850c717bd5701
Nick Melanson <nickm890@gmail.com>
2023-07-09 20:13:25 -0400

    added consumables, and more event handlers for ranged attacks

--- entity_factories.py
+++ entity_factories.py
@@ -1,5 +1,5 @@
 from components.ai import HostileEnemy
-from components.consumable import HealingConsumable
+from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
 from entity import Actor, Item
@@ -9,4 +9,7 @@ player = Actor(char='@', color=(255, 255, 255), name='Player', ai_cls=HostileEne
 orc = Actor(char='o', color=(63, 127, 63), name='Orc', ai_cls=HostileEnemy, fighter=Fighter(hp=10, defense=0, power=3), inventory=Inventory(capacity=0))
 troll = Actor(char='T', color=(0, 127, 0), name='Troll', ai_cls=HostileEnemy, fighter=Fighter(hp=16, defense=1, power=4), inventory=Inventory(capacity=0))
 
-health_potion = Item(char='!', color=(127, 0, 255), name='Health Potion', consumable=HealingConsumable(amount=4))
+confusion_scroll = Item(char='~', color=(255, 0, 0), name='Confusion Scroll', consumable=consumable.ConfusionConsumable(number_of_turns=10))
+fireball_scroll = Item(char='~', color=(255, 0, 0), name='Fireball Scroll', consumable=consumable.FireballDamageConsumable(damage=12, radius=3))
+health_potion = Item(char='!', color=(127, 0, 255), name='Health Potion', consumable=consumable.HealingConsumable(amount=4))
+lightning_scroll = Item(char='~', color=(255, 255, 0), name='Lightning Scroll', consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5))
