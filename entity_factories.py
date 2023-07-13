054935cc9bd3e43b7d763aa168f617ce33cd54c4
Nick Melanson <nickm890@gmail.com>
2023-07-13 18:33:13 -0400

    added more dungeon levels, descending, and character leveling/xp

--- entity_factories.py
+++ entity_factories.py
@@ -2,12 +2,13 @@ from components.ai import HostileEnemy
 from components import consumable
 from components.fighter import Fighter
 from components.inventory import Inventory
+from components.level import Level
 from entity import Actor, Item
 
-player = Actor(char='@', color=(255, 255, 255), name='Player', ai_cls=HostileEnemy, fighter=Fighter(hp=30, defense=2, power=5), inventory=Inventory(capacity=26))
+player = Actor(char='@', color=(255, 255, 255), name='Player', ai_cls=HostileEnemy, fighter=Fighter(hp=30, defense=2, power=5), inventory=Inventory(capacity=26), level=Level(level_up_base=200))
 
-orc = Actor(char='o', color=(63, 127, 63), name='Orc', ai_cls=HostileEnemy, fighter=Fighter(hp=10, defense=0, power=3), inventory=Inventory(capacity=0))
-troll = Actor(char='T', color=(0, 127, 0), name='Troll', ai_cls=HostileEnemy, fighter=Fighter(hp=16, defense=1, power=4), inventory=Inventory(capacity=0))
+orc = Actor(char='o', color=(63, 127, 63), name='Orc', ai_cls=HostileEnemy, fighter=Fighter(hp=10, defense=0, power=3), inventory=Inventory(capacity=0), level=Level(xp_given=35))
+troll = Actor(char='T', color=(0, 127, 0), name='Troll', ai_cls=HostileEnemy, fighter=Fighter(hp=16, defense=1, power=4), inventory=Inventory(capacity=0), level=Level(xp_given=100))
 
 confusion_scroll = Item(char='~', color=(255, 0, 0), name='Confusion Scroll', consumable=consumable.ConfusionConsumable(number_of_turns=10))
 fireball_scroll = Item(char='~', color=(255, 0, 0), name='Fireball Scroll', consumable=consumable.FireballDamageConsumable(damage=12, radius=3))
